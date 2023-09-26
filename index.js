b2a5da48009770c3edea03d9abedcdbb81b8d35c
Din <Dinz5005@gmail.com>
2023-09-27 02:13:59 +0300

    port

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
