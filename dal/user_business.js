0affcca3b48001d046efb8624f54b0bbec6e846b
Din <Dinz5005@gmail.com>
2023-09-26 22:37:41 +0300

    changedAtlas

--- dal/user_business.js
+++ dal/user_business.js
@@ -5,6 +5,6 @@ mongoose.set("strictQuery", false);
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb+srv://DIn:cslWwZeQ0YSDOrrK@b-card-db.uakvttn.mongodb.net/");
+  await mongoose.connect("mongodb+srv://Din:GlkDWFgANjkgdUXm@react.5l3zryn.mongodb.net/");
   console.log("Connected To MongoDB");
 }
