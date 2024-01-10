55951886de15d4faf3ba8e70553e5fb284776ca3
Din <Dinz5005@gmail.com>
2024-01-10 15:09:29 +0200

    build_update

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const cors = require("cors")
-const morgan = require('morgan')
-require("./dal/user_business");
-const user = require("./routes/user");
-const business = require("./routes/business");
+const cors = require('cors');
+const morgan = require('morgan');
+require('./dal/user_business');
+const user = require('./routes/user');
+const business = require('./routes/business');
 const path = require('path');
 const port = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
-app.use(cors({ origin: '*' }))
-app.use(morgan('tiny'))
-app.use("/user", user);
-app.use("/business", business);
+app.use(cors({origin: '*'}));
+app.use(morgan('tiny'));
+app.use('/user', user);
+app.use('/business', business);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
